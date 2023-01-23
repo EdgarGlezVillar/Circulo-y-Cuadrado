d78da48714162521a6f44986669b35a48a70645e
EdgarGlezVillar <122295265+EdgarGlezVillar@users.noreply.github.com>
2023-01-23 13:32:56 -0600

    Add files via upload
    170142 Edgar González Villar

--- Principal.java
+++ Principal.java
@@ -1,15 +1,70 @@
 package CUADRADOPCKG;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		Cuadrado cuadrado1 = new Cuadrado();
-		System.out.println(cuadrado1.calcularDiagonal());
-		System.out.println(cuadrado1.calcularArea());
-		System.out.println(cuadrado1.calcularPerimetro());
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ingrese el método que desea correr");
+		String metodo = scan.nextLine();
 		
-		Circulo circulo1 = new Circulo();
-		System.out.println(circulo1.calcularArea());
-		System.out.println(circulo1.calcularCircunferencia());
+		if(metodo.contains("cuadrado")){
+			System.out.println("Ingrese la medida del lado");
+			String ladoa = scan.nextLine();
+			Integer ladoint = Integer.parseInt(ladoa);
+			double lado = ladoint;
+			Cuadrado cuadrado1 = new Cuadrado(lado);
+			
+			System.out.println("Ingrese lo que se desea calcular");
+			String calc = scan.nextLine();
+			
+			if(calc.contains("area")) {
+				double area = cuadrado1.calcularArea();
+				int areaint = (int) area;
+				System.out.println(area);
+				System.out.println(areaint);
+			}
+			else if(calc.contains("diagonal")) {
+				double diagonal = cuadrado1.calcularDiagonal();
+				int diagonalint = (int) diagonal;
+				System.out.println(diagonal);
+				System.out.println(diagonalint);
+			}
+			else {
+				double perimetro = cuadrado1.calcularPerimetro();
+				int perimetroint = (int) perimetro;
+				System.out.println(perimetro);
+				System.out.println(perimetroint);
+			}
+		
+		}
+		
+	
+		else if(metodo.contains("circulo")){
+			System.out.println("Ingrese la medida del radio");
+			String radioa = scan.nextLine();
+			Integer radio = Integer.parseInt(radioa);
+			Circulo circulo1 = new Circulo(radio);
+			
+			System.out.println("Ingrese lo que se desea calcular");
+			String calc = scan.nextLine();
+			
+			if(calc.contains("area")) {
+				double area = circulo1.calcularArea();
+				int areaint = (int) area;
+				System.out.println(area);
+				System.out.println(areaint);
+
+				
+			}
+			else {
+				double circunferencia = circulo1.calcularCircunferencia();
+				int circunferenciaint = (int) circunferencia;
+				System.out.println(circunferencia);
+				System.out.println(circunferenciaint);
+			}
+						
+		}
 	}
 }
